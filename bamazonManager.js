8633437f6f2e9ae2a6f6f2b2511181002a618932
unknown <naultmichael1@gmail.com>
2019-06-01 08:21:10 -0400

    manager display current

--- bamazonManager.js
+++ bamazonManager.js
@@ -14,3 +14,24 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
 });
+// Function to display current stock
+function displayProducts() {
+    connection.query("SELECT * FROM products", function(error, results) {
+        if (error) throw error;
+        console.log("Complete stock")
+        // Using table npm to display stock
+        var table = new Table({
+            head: ['Product ID', 'Product Name', 'Department', 'Price', 'Quantity']
+          , colWidths: [10, 10, 20, 10, 10],
+          colAligns: ["center", "left", "left", "left", "center"],
+          style: {
+              head: ["teal"],
+              compact: true
+          }
+        });
+        for(let i = 0; i < results.length; i++) {
+            table.push([results[i].id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity]);
+        }
+        console.log(table.toString());
+    })
+}
