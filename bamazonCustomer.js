39d3a2edf12574ae54b3cc710a9bc04920e8d6b4
unknown <naultmichael1@gmail.com>
2019-05-31 21:25:26 -0400

    quantity input

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -24,8 +24,8 @@ var showProducts = function() {
     // });
     var table = new Table({
         head: ['Product ID', 'Product Name', 'Department', 'Price', 'Quantity']
-      , colWidths: [20, 30, 20, 10, 10],
-      colAligns: ["center", "left", "left"],
+      , colWidths: [10, 10, 20, 10, 10],
+      colAligns: ["center", "left", "left", "left", "center"],
       style: {
           head: ["teal"],
           compact: true
@@ -35,8 +35,51 @@ var showProducts = function() {
         table.push([res[i].id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity])
     }
     console.log(table.toString());
-    console.log("")
+    console.log("");
+    beginShop();
 
 });
 };
+
+var beginShop = function() {
+    inquirer.prompt({
+        name: "productBuy",
+        type: "input",
+        message: "Please, please buy something..Select an ID!"
+    })
+    // Choosing an item id to purchase
+    .then(function(answers) {
+        var selection = answers.productBuy;
+        connection.query("SELECT * FROM products WHERE id = ?", selection, function(err, res) {
+            if (err) throw err;
+            if (res.length === 0) {
+                console.log("That Product doesn't exist, Please enter a product from the list above.");
+                // beginShop();
+            }else {
+                // console.log("All is okay!");
+                inquirer.prompt({
+                    name: "newQuantity",
+                    type: "input",
+                    message: "How many items would you like to buy?"
+                })
+                // Telling the buyer if we have that much in stock
+                .then(function(answers2) {
+                    var amount = answers2.quantity;
+                    if (amount > res[0].stock_quantity) {
+                        console.log("Sorry we only have " + res[0].stock_quantity + " items of the selected product");
+                        // beginShop()
+                    }else {
+                        console.log("");
+                        console.log(res[0].product_name + " bought");
+                        console.log(quantity + " qty @ $" + res[0].price )
+
+
+                    }
+                })
+            }
+        }); 
+    });
+};
+
+
 showProducts();
