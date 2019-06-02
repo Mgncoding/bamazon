b58253b6167da0155e95ec6b529e72b193689bde
unknown <naultmichael1@gmail.com>
2019-06-02 10:34:00 -0400

    Manager: inventory

--- bamazonManager.js
+++ bamazonManager.js
@@ -16,22 +16,72 @@ connection.connect(function (err) {
 });
 // Function to display current stock
 function displayProducts() {
-    connection.query("SELECT * FROM products", function(error, results) {
+    connection.query("SELECT * FROM products", function (error, results) {
         if (error) throw error;
         console.log("Complete stock")
         // Using table npm to display stock
         var table = new Table({
             head: ['Product ID', 'Product Name', 'Department', 'Price', 'Quantity']
-          , colWidths: [10, 10, 20, 10, 10],
-          colAligns: ["center", "left", "left", "left", "center"],
-          style: {
-              head: ["teal"],
-              compact: true
-          }
+            , colWidths: [10, 10, 20, 10, 10],
+            colAligns: ["center", "left", "left", "right", "center"],
+            style: {
+                head: ["teal"],
+                compact: true
+            }
         });
-        for(let i = 0; i < results.length; i++) {
+        for (let i = 0; i < results.length; i++) {
             table.push([results[i].id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity]);
         }
         console.log(table.toString());
+        // saleProducts()
+    })
+}
+// Function for products that can be sold
+function saleProducts() {
+    connection.query("SELECT * FROM products WHERE stock_quantity > 0", function (error, results) {
+        if (error) throw error;
+        console.log("bAmazon Store Inventory");
+
+        // Using table npm to display stock
+        var table = new Table({
+            head: ['Product ID', 'Product Name', 'Department', 'Price', 'Quantity']
+            , colWidths: [10, 10, 20, 10, 10],
+            colAligns: ["center", "left", "left", "right", "center"],
+            style: {
+                head: ["teal"],
+                compact: true
+            }
+        });
+        for (let i = 0; i < results.length; i++) {
+            table.push([results[i].id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity]);
+        }
+        console.log(table.toString());
+        initialPrompt();
+
+    });
+};
+// Function to display low inventory
+function lowInventory() {
+    connection.query("SELECT * FROM products WHERE stock_quantity < 5 ", function(error, results) {
+        if (error) throw error;
+        console.log("Low Inventory")
+
+        // Using table npm to display stock
+        var table = new Table({
+            head: ['Product ID', 'Product Name', 'Department', 'Price', 'Quantity']
+            , colWidths: [10, 10, 20, 10, 10],
+            colAligns: ["center", "left", "left", "right", "center"],
+            style: {
+                head: ["teal"],
+                compact: true
+            }
+        });
+        for (let i = 0; i < results.length; i++) {
+            table.push([results[i].id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity]);
+        }
+        console.log(table.toString());
+        initialPrompt();
+
+
     })
 }
